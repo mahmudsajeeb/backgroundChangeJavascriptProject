defd9755428c8039daf229e85685e51aeeda7cd7
Mahmud sajib <mahmudsajib082@gmail.com>
2023-02-13 22:20:39 +0600

    Background Change

---
+++ app.js
@@ -0,0 +1,16 @@
+// add event listener primary button
+document.getElementById("primary").addEventListener("click",function(){
+  document.body.style.backgroundColor ='#0B5ED7'
+})
+
+//add event listener decondary button change background colour
+
+document.getElementById('secondary').addEventListener('click', function(){
+  document.body.style.backgroundColor ="gray"
+})
+
+//add event listener decondary button change background colour
+
+document.getElementById('danger').addEventListener('click', function(){
+  document.body.style.backgroundColor ="red"
+})
